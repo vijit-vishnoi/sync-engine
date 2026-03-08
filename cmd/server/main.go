999d164ee96045ed4c4f447f47b0a0419d996723
vijit-vishnoi <vishnoivijit@gmail.com>
2026-03-08 19:01:18 +0530

    initialised the main file

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,7 +1,24 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+)
 
 func main() {
-	fmt.Println("hello");
+	port:="8080"
+	r:=chi.NewRouter();
+	r.Use(middleware.Logger);
+	r.Use(middleware.Recoverer);
+	r.Get("/health",func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK);
+		w.Write([]byte("Health check passed"));
+	})
+	fmt.Println("Server is listening to port:",port);
+	err:=http.ListenAndServe(":"+port,r)
+	if err!=nil{
+		fmt.Println("Server failed due to",err);
+	}
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module github.com/vijit-vishnoi
 
 go 1.25.6
+
+require (
+	github.com/go-chi/chi v1.5.5 // indirect
+	github.com/go-chi/chi/v5 v5.2.5 // indirect
+)
