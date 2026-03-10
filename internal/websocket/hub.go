fd533d3dcfc2f12f21175cf8665b85dc6af334b0
vijit-vishnoi <vishnoivijit@gmail.com>
2026-03-10 20:48:05 +0530

    created hub logic

--- internal/websocket/hub.go
+++ internal/websocket/hub.go
@@ -16,3 +16,35 @@ type Client struct {
 	conn *websocket.Conn
 	send chan []byte
 }
+func NewHub() *Hub{
+	h:=&Hub{
+		register: make(chan *Client),
+		unregister: make(chan *Client),
+		clients: make(map[*Client]bool),
+		broadcast: make(chan []byte),
+	}
+	return h
+}
+func (h *Hub)Run(){
+	for{
+		select {
+		case client:=<-h.register:
+			h.clients[client]=true;
+		case client:=<-h.unregister:
+			if _,ok:=h.clients[client];ok{
+				delete(h.clients,client)
+				close(client.send)
+			}
+			 
+		case message:=<-h.broadcast:
+			for client:=range h.clients{
+				select {
+				case client.send<-message:
+				default:
+					close(client.send)
+					delete(h.clients,client)
+				}
+			}
+		}
+	}
+}
